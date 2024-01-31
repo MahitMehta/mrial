0e7b21c812db6b409939cdb6907a1081b9bb2136
mahitmehta <mahit.py@gmail.com>
2024-01-31 00:04:15 -0500

    add perms

--- macos/scripts/build.sh
+++ macos/scripts/build.sh
@@ -33,8 +33,5 @@ rm -rf $MACOS_RUST_APP_NAME/.Trashes
 
 FULL_NAME=$MACOS_RUST_APP_NAME
 
-epochdate=$(($(date +'%s * 1000 + %-N / 1000000')))
-tcc_service_appleevents="replace into access (service,client,client_type,auth_value,auth_reason,auth_version,indirect_object_identifier_type,indirect_object_identifier,flags,last_modified) values (\"kTCCServiceAppleEvents\",\"/usr/sbin/sshd\",1,2,4,1,0,\"com.apple.finder\",0,$epochdate);"
-sudo sqlite3 "/Users/distiller/Library/Application Support/com.apple.TCC/TCC.db" "$tcc_service_appleevents"
 create-dmg --window-size 500 500 --app-drop-link 370 200 --icon-size 125 --icon $FULL_NAME.app 120 200 $FULL_NAME.dmg $MACOS_RUST_APP_NAME
 rm -rf $MACOS_RUST_APP_NAME
