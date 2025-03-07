715d94c1cce0fee790cb8a8aa9d8434ee8fada6d
Mahit Mehta <mahit.py@gmail.com>
2025-03-07 01:57:04 -0500

    Installed Opus

--- mrial_server/src/audio/encoder.rs
+++ mrial_server/src/audio/encoder.rs
@@ -1,38 +1,19 @@
-use flacenc::{config::Encoder, source::Source};
+use opus::{Channels, Encoder};
 
 pub struct AudioEncoder {
-    encoder: Encoder,
-    channels: usize,
-    bits_per_sample: usize,
-    sample_rate: usize,
+    encoder: Encoder
 }
 
 impl AudioEncoder {
-    pub fn new(channels: usize, bits_per_sample: usize, sample_rate: usize) -> Self {
-        Self {
-            encoder: flacenc::config::Encoder::default(),
-            channels,
-            bits_per_sample,
-            sample_rate,
-        }
+    pub fn new(
+        sample_rate: u32,
+        channels: Channels) -> Result<Self, Box<dyn std::error::Error>> {
+        Ok(Self {
+            encoder: Encoder::new(sample_rate, channels, opus::Application::LowDelay)?,
+        })
     }
 
     pub fn encode(&self, samples: &[i32]) {
-        println!("Block {}", self.encoder.block_sizes[0]);
-        let source = flacenc::source::MemSource::from_samples(
-            samples,
-            self.channels,
-            self.bits_per_sample,
-            self.sample_rate,
-        );
-
-        let flac_stream = flacenc::encode_with_fixed_block_size(
-            &self.encoder,
-            source,
-            self.encoder.block_sizes[0],
-        )
-        .expect("Audio Encoding failed.");
-
-        println!("count: {}", flac_stream.frame_count());
+      
     }
 }
